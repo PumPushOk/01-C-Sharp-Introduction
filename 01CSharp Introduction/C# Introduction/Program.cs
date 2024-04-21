860d1e2803daf94fd8ded4f76e50aa72a21e0413
Олег Парнета <mrdastinol@gmail.com>
2024-04-21 16:10:23 +0300

    Fixed Task1

--- 01CSharp Introduction/C# Introduction/Program.cs
+++ 01CSharp Introduction/C# Introduction/Program.cs
@@ -6,7 +6,52 @@ namespace C__Introduction
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Start Program");
+			Task1();
+			Console.Clear();
+			Task2();
+			Console.Clear();
+		}
+		static void Task1()
+		{
+			Console.Write("Enter a number: ");
+			int num;
+			num = int.Parse(Console.ReadLine());
+
+			if (num < 1 || num > 100)
+			{
+				Console.WriteLine("Error!");
+				return;
+			}
+
+			bool isMultipleOfThree = false;
+			if (num % 3 == 0)
+				isMultipleOfThree = true;
+
+			bool isMultipleOfFive = false;
+			if (num % 5 == 0)
+				isMultipleOfFive = true;
+
+			if (isMultipleOfThree)
+				Console.Write("Fizz");
+			if (isMultipleOfFive)
+				Console.Write("Buzz");
+			if (!isMultipleOfThree && !isMultipleOfFive)
+				Console.Write(num);
+			Console.WriteLine();
+		}
+		static void Task2()
+		{
+		
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
