e54242155d10ffc6327eb946e09bb2e1ac690f8a
Олег Парнета <mrdastinol@gmail.com>
2024-05-23 12:31:24 +0300

    Task5 done

---
+++ 03StringAndStatic/03StringAndStatic/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _03StringAndStatic
+{
+	static class Task5
+	{
+		public static void Run()
+		{
+			Console.Write("Enter a statement(with spaces): ");
+			string[] statement = Console.ReadLine().Split();
+			int num1 = int.Parse(statement[0]);
+			char operation = char.Parse(statement[1]);
+			int num2 = int.Parse(statement[2]);
+			int result = 0;
+			if (operation == '+') 
+				result = num1 + num2;
+			else if(operation == '-')
+				result = num1 - num2;
+			Console.WriteLine("Result is " + result);
+		}
+	}
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Task5.Run();
+		}
+	}
+}
