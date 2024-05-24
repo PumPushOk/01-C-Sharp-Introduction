c4d1d2565e5c5754ed28781218bcc29045f846a2
Олег Парнета <mrdastinol@gmail.com>
2024-05-24 19:09:41 +0300

    Task6 done

--- 03StringAndStatic/03StringAndStatic/Program.cs
+++ 03StringAndStatic/03StringAndStatic/Program.cs
@@ -10,11 +10,34 @@
 			char operation = char.Parse(statement[1]);
 			int num2 = int.Parse(statement[2]);
 			int result = 0;
-			if (operation == '+') 
+			if (operation == '+')
 				result = num1 + num2;
-			else if(operation == '-')
+			else if (operation == '-')
 				result = num1 - num2;
 			Console.WriteLine("Result is " + result);
+		}
+	}
+	static class Task6
+	{
+		public static void Run()
+		{
+			Console.WriteLine("Enter a text:");
+			string text = Console.ReadLine();
+			bool isBeginning = true;
+			for (int i = 0; i < text.Length; i++)
+			{
+				if (char.IsAsciiLetter(text[i]) && isBeginning)
+				{
+					Console.Write(char.ToUpper(text[i]));
+					isBeginning = false;
+				}
+				else
+				{
+					Console.Write(text[i]);
+					if(text[i] == '.')
+						isBeginning = true;
+				}
+			}
 		}
 	}
 	internal class Program
@@ -22,6 +45,8 @@
 		static void Main(string[] args)
 		{
 			//Task5.Run();
+			//Task6.Run();
 		}
+
 	}
 }
