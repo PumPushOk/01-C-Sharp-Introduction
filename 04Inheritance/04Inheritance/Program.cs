3d88d36bb2b4bfa5499728d2133d13f441bd258c
Олег Парнета <mrdastinol@gmail.com>
2024-05-27 14:44:53 +0300

    Task1 done

---
+++ 04Inheritance/04Inheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _04Inheritance
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//Завдання 1
+			//Money money = new Money(100, 40);
+			//Product product = new Product(money, "Laptop");
+			//Console.WriteLine("Product name: " + product.Name);
+			//Console.WriteLine("Product price: " + product);
+			//product.DecreaseSum(50.32);
+			//Console.WriteLine("Product price: " + product);
+
+			//Завдання 2
+			//Kettle kettle = new Kettle("Kettle", "Boiling water", "*whistle*");
+			//kettle.Show();
+			//Console.Write(" - ");
+			//kettle.Desc();
+			//Console.Write(" - ");
+			//kettle.PlaySound();
+			//Console.WriteLine();
+			//Auto auto = new Auto("Car", "Riding", "*Brrrrr*");
+			//auto.Show();
+			//Console.Write(" - ");
+			//auto.Desc();
+			//Console.Write(" - ");
+			//auto.PlaySound();
+
+			//Завдання 3
+
+			//Завдання 4
+			//President president = new President("Macho", "ruling");
+			//president.Work();
+		}
+	}
+}
---
+++ 04Inheritance/04Inheritance/Task2.cs
@@ -0,0 +1,44 @@
+﻿namespace _04Inheritance
+{
+	class Device
+	{
+		string name;
+		string description;
+		string sound;
+		public Device(string name, string description, string sound)
+		{
+			this.name = name;
+			this.description = description;
+			this.sound = sound;
+		}
+
+		public void Show()
+		{
+			Console.Write(name);
+		}
+		public void Desc()
+		{
+			Console.Write(description);
+		}
+		public void Sound()
+		{
+			Console.Write(sound);
+		}
+	}
+	class Kettle : Device
+	{
+		public Kettle(string name, string description, string sound) : base(name, description, sound) { }
+	}
+	class Microwave : Device
+	{
+		public Microwave(string name, string description, string sound) : base(name, description, sound) { }
+	}
+	class Auto : Device
+	{
+		public Auto(string name, string description, string sound) : base(name, description, sound) { }
+	}
+	class Ship : Device
+	{
+		public Ship(string name, string description, string sound) : base(name, description, sound) { }
+	}
+}
