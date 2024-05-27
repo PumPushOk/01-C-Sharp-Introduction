3547409ad8f651924dc00b7fbd0f34b7bbf2381f
Олег Парнета <mrdastinol@gmail.com>
2024-05-27 09:22:45 +0300

    Homework done

--- 03StringAndStatic/03StringAndStatic/Program.cs
+++ 03StringAndStatic/03StringAndStatic/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _03StringAndStatic
+﻿using System.Net.NetworkInformation;
+
+namespace _03StringAndStatic
 {
 	static class Task5
 	{
@@ -34,9 +36,70 @@
 				else
 				{
 					Console.Write(text[i]);
-					if(text[i] == '.')
+					if (text[i] == '.')
 						isBeginning = true;
 				}
+			}
+		}
+	}
+	static class Task7
+	{
+		private static string GetStars(int starsCount)
+		{
+			string result = string.Empty;
+			while (starsCount > 0)
+			{
+				result += '*';
+				starsCount--;
+			}
+			return result;
+		}
+		public static void Run()
+		{
+			Console.WriteLine("Enter a text:");
+			//string text = Console.ReadLine();
+			string text = "To be, or not to be, that is the question, Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take arms against a sea of troubles, And by opposing end them? To die: to sleep; No more; and by a sleep to say we end The heart-ache and the thousand natural shocks That flesh is heir to, 'tis a consummation Devoutly to be wish'd. To die, to sleep";
+			Console.WriteLine(text);
+			Console.WriteLine();
+
+			string[] badWords = { "die", "suffer", "to" };
+
+			int[] statistics = new int[badWords.Length];
+			string result = string.Empty;
+			while (result.Length != text.Length)
+			{
+				string nextWord = string.Empty;
+				int nextWordIndex = text.Length;
+				foreach (string word in badWords)
+				{
+					int index = text.IndexOf(word, result.Length);
+					if (index < nextWordIndex && index != -1)
+					{
+						nextWord = word;
+						nextWordIndex = index;
+					}
+				}
+
+				result += text.Substring(result.Length, nextWordIndex - result.Length);
+				result += GetStars(nextWord.Length);
+
+				for (int i = 0; i < badWords.Length; i++)
+					if (badWords[i] == nextWord)
+					{
+						statistics[i]++;
+						break;
+					}
+			}
+
+			Console.WriteLine("Redacted text:");
+			Console.WriteLine(result);
+
+			Console.WriteLine();
+			Console.WriteLine("Statistics:");
+			for (int i = 0; i < statistics.Length; i++)
+			{
+				if (statistics[i] != 0)
+					Console.WriteLine($"\t{statistics[i]} replacement of word \"{badWords[i]}\"");
 			}
 		}
 	}
@@ -46,6 +109,7 @@
 		{
 			//Task5.Run();
 			//Task6.Run();
+			//Task7.Run();
 		}
 
 	}
